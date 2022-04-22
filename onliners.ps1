1cec7b5fd7e0570dca435b49fc44f3fabc7848a9
Sam A <s.f.anderson11@gmail.com>
2022-04-22 07:45:40 -0700

    Update onliners.ps1
    add comment

--- onliners.ps1
+++ onliners.ps1
@@ -1,5 +1,5 @@
-﻿1..254 | % {"127.0.0.$($_): $(Test-Connection -count 1 -comp 127.0.0.$($_) -quiet)"}
-
+# sends a ping request to loop back... 
+1..254 | % {"127.0.0.$($_): $(Test-Connection -count 1 -comp 127.0.0.$($_) -quiet)"}
 
 #Roughly translates to
 ForEach ($num in 1..5) {
